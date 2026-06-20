9b127d246deb983c46352787d6d7de8722c62da7
Aryan Singh Nagar <asn.dyrnwyn@gmail.com>
2026-06-20 10:30:42 +0530

    build: remove legacy Android project directory from tracking

--- privacy_chat_android/app/src/main/java/com/example/privacychat/NavigationKeys.kt
+++
@@ -1,9 +0,0 @@
-package com.example.privacychat
-
-import androidx.navigation3.runtime.NavKey
-import kotlinx.serialization.Serializable
-
-@Serializable data object Config : NavKey
-@Serializable data object Auth : NavKey
-@Serializable data object Chat : NavKey
-
